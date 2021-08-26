31ec0e82029a2460653d4165ac213adac200f598
access <tommi_versetti@list.ru>
2021-08-26 17:10:15 +0300

    initial commit

--- js/content/eventListener.js
+++ js/content/eventListener.js
@@ -14,45 +14,6 @@ window.addEventListener('message', function (event) {
           }, undefined, { shallow: true })
           event.preventDefault();
           event.stopPropagation();
-
-          // var request = new XMLHttpRequest();
-          // request.onreadystatechange = function () {
-          //   if (request.readyState < 4) {
-          //     return;
-          //   }
-          //   if (request.status !== 200) {
-          //     return;
-          //   }
-          //   if (request.readyState === 4) {
-          //     successCallback();
-          //   }
-          // };
-          // request.open('GET', route, true);
-          // request.send('');
-          // successCallback = function () {
-          //   //document.innerHTML = request.responseText;
-          //   //history.pushState('', '', "?" + container + "=" + source);
-
-          //   let doc = document.implementation.createHTMLDocument("");
-          //   doc.open();
-          //   doc.write(request.responseText);
-          //   doc.close();
-
-          //   document.head.innerHTML = doc.head.innerHTML;
-          //   document.body.innerHTML = doc.body.innerHTML;
-          //   document.title = doc.title;
-
-          //   const nextURL = route;
-          //   const nextTitle = doc.title;
-          //   const nextState = { additionalInformation: '...' };
-          //   window.history.pushState(nextState, nextTitle, nextURL);
-          //   window.history.replaceState(nextState, nextTitle, nextURL);
-
-          //   console.log('request.responseText: ', doc);
-          // }
-
-
-
           break;
 
         default:
